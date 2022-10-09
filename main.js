fef513592ec1778b5d6fe800c433eec95882f121
Raj Heer <thisisrajmatazz@gmail.com>
2022-10-09 17:01:51 +0100

    Add playRound function

--- main.js
+++ main.js
@@ -10,3 +10,31 @@ function getPlayChoice () {
     let playChoice = prompt("Enter your choice - 'r', 'p', or 's'?");
     console.log(playChoice);
 }
+
+function playRound () {
+    let compChoice = getCompChoice();
+    let playChoice = getPlayChoice().toLowerCase();
+
+    if (compChoice === playChoice) {
+        console.log(`You both drew with ${compChoice}`);
+        return "draw";
+    } else if (compChoice === weapons[0] && playChoice === weapons[2]) {
+        console.log("You lose! Rock beat scissors.");
+        return "lose";
+    } else if (compChoice === weapons[0]  && playChoice === weapons[1]) {
+        console.log("You win! Paper beat rock.");
+        return "win";
+    } else if (compChoice === weapons[2]  && playChoice === weapons[1]) {
+        console.log("You lose! Scissors beat paper.");
+        return "lose";
+    } else if ( compChoice === weapons[2]  && playChoice === weapons[0]) {
+        console.log("You win! Rock beats scissors");
+        return "win";
+    } else if (compChoice === weapons[1]  && playChoice === weapons[0]) {
+        console.log("You lose! Paper beats rock.");
+        return "lose";
+    } else if (compChoice === weapons[1]  && playChoice === weapons[2]) {
+        console.log("You win! Scissors beat paper");
+        return "win";
+    }
+}
