f5e3df5deb290f419d41f5db21e5485685c1898b
Raj Heer <thisisrajmatazz@gmail.com>
2022-10-09 16:58:10 +0100

    Add function to get player choice.

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ const weapons = ['r','p','s']
 function getCompChoice () {
     let num = Math.floor(Math.random() * 3);
     let compChoice = weapons[num];
-    console.log(compChoice);
+    return compChoice;
+}
+
+function getPlayChoice () {
+    let playChoice = prompt("Enter your choice - 'r', 'p', or 's'?");
+    console.log(playChoice);
 }
